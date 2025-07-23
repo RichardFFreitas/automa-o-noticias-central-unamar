6d8b5eee694ef5a15ef178fe33213e3f64fc3204
Richard <richardfelipelives@gmail.com>
2025-07-23 20:33:35 -0300

    chore: att start.sh

--- start.sh
+++ start.sh
@@ -10,12 +10,12 @@ fi
 
 # Instala dependências do sistema necessárias ao Playwright (caso esteja num container Ubuntu slim)
 apt-get update && apt-get install -y \
-    libglib2.0-0 libgobject-2.0-0 libnspr4 libnss3 libnssutil3 libsmime3 \
-    libgio-2.0-0 libdbus-1-3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libexpat1 \
-    libxcb1 libxkbcommon0 libatspi2.0-0 libx11-6 libxcomposite1 libxdamage1 \
-    libxext6 libxfixes3 libxrandr2 libgbm1 libcairo2 libpango-1.0-0 libasound2 \
-    fonts-liberation libappindicator3-1 libxshmfence1 lsb-release xdg-utils wget \
-    ca-certificates --no-install-recommends
+    libglib2.0-0 libnspr4 libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 \
+    libexpat1 libx11-6 libxcomposite1 libxdamage1 libxext6 libxfixes3 \
+    libxrandr2 libgbm1 libcairo2 libpango-1.0-0 libasound2 fonts-liberation \
+    libappindicator3-1 libxshmfence1 xdg-utils wget ca-certificates \
+    --no-install-recommends
+
 
 # Instala o navegador Chromium (headless) para o Playwright
 python -m playwright install chromium
